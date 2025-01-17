4b34bdb3ec52d9c07dee359383c27d23cd457340
Sebastian <sl5softwarelab@gmail.com>
2025-01-17 02:14:37 +0100

    updates

---
+++ wildfiregames.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name     Greasemonkey wildfiregames
+// @namespace    https://wildfiregames.com/forum/
+// @version  1.0
+// @description  Greasemonkey wildfiregames
+// @match   https://wildfiregames.com/
+// @grant    none
+// ==/UserScript==
+
+
+(function(){
+
+  window.location.href = 'https://wildfiregames.com/forum/';
+
+})()
+
