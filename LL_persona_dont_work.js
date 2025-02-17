4493fc852ebdf045ebd5937ebed0c735f6daed0e
sl5net <sl5net@users.noreply.github.com>
2025-02-17 18:08:25 +0100

    Create LL_persona_dont_work.js

---
+++ LL_persona_dont_work.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name     GM LL persona dont work
+// @namespace    https://wildfiregames.com/forum/
+// @version  1.0
+// @description  Greasemonkey wildfiregames
+// @match   https://www.linkedin.com/verify/identity/persona/start*
+// @match   https://www.linkedin.com/verify/identity/persona*
+// @match   https://www.linkedin.com/verify/identity*
+// @grant    none
+// ==/UserScript==
+
+// https://www.linkedin.com/verify/identity/persona/start/?entryPoint=selfview_topcard&platform=DESKTOP&referrer=verify_hub_redirect
+
+// ich habe keinen Reisepass. Und ich brauche auch keinen. Mir reicht ein Persolanlausweis. Aber es will einen. Also für mich nutzlos. Erfüllt seinen Zweck der identitication nicht.
+
+// I don't have a passport. And I don't need one. But it wants one. An identity card is enough for me. So it's useless for me. It doesn't fulfill its purpose of identification.
+
+
+(function(){
+
+  // window.location.href = 'https://www.linkedin.com/mypreferences/d/categories/account';
+  window.location.href = 'https://social.tchncs.de/home';
+  
+
+})()
